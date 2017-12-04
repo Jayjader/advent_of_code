7f05facee2d2bd592835bc742cd4a9cdba6230a7
Jayjader <njay47@gmail.com>
2017-12-04 21:07:41 +0100

    Changed triple apostrophes to triple quotes for docstrings

--- 3.py
+++ 3.py
@@ -1,8 +1,8 @@
 def ring_size_from_position(position: int) -> int:
-    '''
+    """
     Computes which ring the position falls on.
     Ring size is determined by the length of its sides (so ring sizes increase 2 by 2).
-    '''
+    """
     x = 1
     while x ** 2 < position:
         x += 2
@@ -11,19 +11,19 @@ def ring_size_from_position(position: int) -> int:
 
 
 def position_on_ring(global_position, ring_size: int) -> int:
-    '''
+    """
     Computes the local/relative position on a ring.
     Positions go from 0 (bottom right corner) to 4*(ring_size - 1)
-    '''
+    """
     ring_pos = ring_size ** 2 - global_position
     # print(f'Position on ring of size {ring_size}: {ring_pos}')
     return ring_pos
 
 
 def distance_from_center_of_ring_side(position_on_ring, ring_size: int) -> int:
-    '''
+    """
     Computes a position's offset from the center of the side of it's associated ring.
-    '''
+    """
     if ring_size == 1:
         return 0
     side = position_on_ring // (ring_size - 1)
@@ -56,10 +56,3 @@ ring_size = ring_size_from_position(data_position)
 
 print(distance_from_center(distance_from_center_of_ring_side(position_on_ring(data_position,
                                                                               ring_size), ring_size), ring_size))
-
-spiral_index = 0
-current_ring_size = 1
-
-
-def ring_length(ring_size: int) -> int:
-    return 4 * ring_size - 4
