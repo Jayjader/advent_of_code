4db09fcd68684b87342364d6a79ede9e98714423
Jayjader <njay47@gmail.com>
2017-12-04 23:19:42 +0100

    Day 3, part 2

--- 3.py
+++ 3.py
@@ -1,3 +1,8 @@
+from collections import defaultdict
+from enum import Enum
+from typing import List, Tuple
+
+
 def ring_size_from_position(position: int) -> int:
     """
     Computes which ring the position falls on.
@@ -56,3 +61,60 @@ ring_size = ring_size_from_position(data_position)
 
 print(distance_from_center(distance_from_center_of_ring_side(position_on_ring(data_position,
                                                                               ring_size), ring_size), ring_size))
+
+
+# Part Two
+
+class Direction(tuple, Enum):
+    RIGHT = (1, 0)
+    UP = (0, 1)
+    LEFT = (-1, 0)
+    DOWN = (0, -1)
+
+    def next(self):
+        cls = self.__class__
+        members = list(cls)
+        index = (members.index(self) + 1) % len(members)
+        return members[index]
+
+
+def neighbours(coords: Tuple[int, int]) -> List[Tuple[int, int]]:
+    output = []
+    x, y = coords
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if not (dx == dy == 0):
+                output.append((x + dx, y + dy))
+
+    return output
+
+
+def add(t1, t2: Tuple[int, int]) -> Tuple[int, int]:
+    t2 = tuple(t2)
+    return t1[0] + t2[0], t1[1] + t2[1]
+
+
+grid = defaultdict(int)
+
+current_val = 1
+current_index = 0
+current_wrapping_direction = Direction.RIGHT
+next_wrapping_direction = current_wrapping_direction.next()
+current_coords = (0, 0)
+
+while True:
+    grid[current_coords] = current_val
+
+    if grid[add(current_coords, next_wrapping_direction)] == 0:
+        current_wrapping_direction = next_wrapping_direction
+        next_wrapping_direction = current_wrapping_direction.next()
+
+    current_coords = add(current_coords, current_wrapping_direction)
+    current_val = 0
+    for cell in neighbours(current_coords):
+        current_val += grid[cell]
+
+    if current_val > data_position:
+        break
+
+print(current_val)
