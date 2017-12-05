b5f041abcdaca5c23922727855b1eff3143acb87
Jayjader <njay47@gmail.com>
2017-12-05 13:54:34 +0100

    Day 5, part 2

--- 5.py
+++ 5.py
@@ -24,3 +24,30 @@ while 0 <= current_position < number_of_offsets:
     steps += 1
 
 print(steps)
+
+
+# Part Two
+
+def step2(offsets: List[int], position: int) -> int:
+    """
+    Computes the next jump in the interrupt handling, and returns the new position
+    """
+    offset = offsets[position]
+    offsets[position] += 1 if offset < 3 else -1
+    return position + offset
+
+
+with open('offsets.txt') as input_file_desc:
+    offsets = input_file_desc.readlines()
+
+offsets = list(map(int, map(str.strip, offsets)))
+
+steps = 0
+number_of_offsets = len(offsets)
+current_position = 0
+
+while 0 <= current_position < number_of_offsets:
+    current_position = step2(offsets, current_position)
+    steps += 1
+
+print(steps)
