e10ab2370f76042b8d0779f8526675b1e3f2d372
Jayjader <njay47@gmail.com>
2017-12-06 16:26:26 +0100

    Day 6, part 2
    Removed debug messages from part 1.
    Fixed `reallocate()` docstring formatting.

--- 6.py
+++ 6.py
@@ -1,22 +1,19 @@
-from typing import List, Set, Tuple
+from typing import Dict, List, Set, Tuple
 
 
 def reallocate(memory_allocation: List[int]) -> None:
     """
     Computes the next reallocation cycle by round-robin redistributing the first, most full memory bank to all memory
     banks.
+
     :param memory_allocation: Tuple[int, ...] Current memory allocation state
     """
     bank_to_reallocate = memory_allocation.index(max(memory_allocation))
     blocks_to_reallocate = memory_allocation[bank_to_reallocate]
-    # print(f'Memory state:\t\t{memory_allocation}')
-    # print(f'Bank to reallocate: {bank_to_reallocate} (contains {blocks_to_reallocate} blocks)')
     memory_allocation[bank_to_reallocate] = 0
     for _ in range(blocks_to_reallocate):
         bank_to_reallocate = (bank_to_reallocate + 1) % len(memory_allocation)
         memory_allocation[bank_to_reallocate] += 1
-
-    # print(f'New memory state:\t{memory_allocation}')
 
 
 already_seen_configurations: Set[Tuple[int, ...]] = set()
@@ -29,3 +26,20 @@ while tuple(bank_allocation) not in already_seen_configurations:
     reallocate(bank_allocation)
 
 print(len(already_seen_configurations))
+
+
+# Part Two
+
+past_configurations: Dict[Tuple[int, ...], int] = {}
+
+with open('memory_banks.txt') as input_file_desc:
+    bank_allocation = [int(blocks) for blocks in input_file_desc.readline().strip().split()]
+
+step = 0
+
+while past_configurations.get(tuple(bank_allocation)) is None:
+    past_configurations[tuple(bank_allocation)] = step
+    reallocate(bank_allocation)
+    step += 1
+
+print(step - past_configurations[tuple(bank_allocation)])
